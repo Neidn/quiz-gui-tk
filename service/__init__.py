49b74cee71f752d471cb150c6fec3d377756184e
Neidn <dty3152@gmail.com>
2024-02-02 11:24:13 +0900

    update question and score

--- service/__init__.py
+++ service/__init__.py
@@ -2,11 +2,32 @@ from .data import question_data
 from .quiz_brain import QuizBrain
 from .ui import QuizUI
 
+
 # data = get_data()
 # print(data)
 
+def next_question():
+    if quiz.still_has_questions():
+        question = quiz.next_question()
+        ui.update_question(question)
+    else:
+        ui.game_over()
+
+
+def check_answer(answer):
+    if quiz.check_answer(answer):
+        ui.update_score(quiz.score)
+    next_question()
+
+
 quiz = QuizBrain(question_data)
 ui = QuizUI()
+ui.set_true_button_command(lambda: check_answer("True"))
+ui.set_false_button_command(lambda: check_answer("False"))
+
+next_question()
+
+ui.mainloop()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- service/ui.py
+++ service/ui.py
@@ -3,6 +3,10 @@ from tkinter import *
 from .config import *
 
 
+def check_answer(current_question, answer):
+    return answer.lower() == current_question.answer.lower()
+
+
 class QuizUI(Tk):
 
     def __init__(self):
@@ -17,8 +21,6 @@ class QuizUI(Tk):
         self.__create_score_label()
         self.__create_canvas()
         self.__create_buttons()
-
-        self.mainloop()
 
     def __create_score_label(self):
         self.score_label = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg=LABEL_COLOR)
@@ -44,8 +46,14 @@ class QuizUI(Tk):
         self.canvas.itemconfig(self.question_text, text=question)
 
     def __create_buttons(self):
-        self.true_button = Button(image=self.true_image, highlightthickness=0)
-        self.true_button.grid(row=2, column=0)
-
         self.false_button = Button(image=self.false_image, highlightthickness=0)
-        self.false_button.grid(row=2, column=1)
+        self.false_button.grid(row=2, column=0)
+
+        self.true_button = Button(image=self.true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=1)
+
+    def set_true_button_command(self, command):
+        self.true_button.config(command=command)
+
+    def set_false_button_command(self, command):
+        self.false_button.config(command=command)
