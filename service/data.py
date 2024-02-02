3259d0382b667456e02b48cf704644be36dfe030
Neidn <dty3152@gmail.com>
2024-02-02 10:01:00 +0900

    get questions from open db

--- service/data.py
+++ service/data.py
@@ -2,10 +2,18 @@ import requests
 
 from .config import *
 
+params = {
+    "amount": QUESTION_AMOUNT,
+    "category": QUESTION_CATEGORY,
+    "type": QUESTION_TYPE
+}
+
 
 def get_data():
-    api_url = f"{QUESTION_API_DEFAULT_URL}?amount={QUESTION_AMOUNT}&category={QUESTION_CATEGORY}&type={QUESTION_TYPE}"
-    response = requests.get(api_url)
+    response = requests.get(
+        QUESTION_API_DEFAULT_URL,
+        params=params,
+    )
 
     # If the response was successful, no Exception will be raised
     response.raise_for_status()
